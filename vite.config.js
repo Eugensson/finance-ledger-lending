0a8312511dfa5412628b468788a69ae456fa6645
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2024-02-09 09:36:57 +0200

    Add Vite Image Optimizer

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
-export default {
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { defineConfig } from 'vite';
+
+const config = defineConfig(() => {
+  return {
+    plugins: [
+      ViteImageOptimizer({
+        /* pass your config */
+      }),
+    ],
     base: '/finance-ledger-lending/'
-}
+  };
+});
+
+export default config;
